e2fa3505a1ee00c85a4e2e88cc2ffbd09ed13c81
Andrei Sestac <andrey_lucian2012@yahoo.com>
2022-04-12 19:11:25 +0300

    fixing sanctum auth

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->increments("id");
             $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
+            $table->string('token', 191)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
