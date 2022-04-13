f73d921dc11a69c9fc093628509fad183e6f3af7
Andrei Sestac <andrey_lucian2012@yahoo.com>
2022-04-13 18:09:54 +0300

    updated users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('api_token', 191)->unique();
+            $table->string('api_token', 191)->default(null)->nullable()->unique();
             $table->timestamps();
         });
     }
