9fa225ebbeb9933b7ead70ce50dae3100811adb7
Andrei Sestac <andrey_lucian2012@yahoo.com>
2022-04-15 18:43:05 +0300

    auth fixing

--- app/Http/Controllers/ApiDummyController.php
+++ app/Http/Controllers/ApiDummyController.php
@@ -38,7 +38,7 @@ class ApiDummyController extends Controller
     }
 
     public function check(Request $request){
-
+        $header = $request->header('Authorization');
 
         return response([
             "Authorization" => $header,
